b35108d774f6829a875edfe54f78ea9775b076b2
sanbelote <sanbelote@users.noreply.github.com>
2019-09-01 10:46:25 +0530

    added test
    File

---
+++ Test.java
@@ -0,0 +1,16 @@
+
+public class Test {
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		for(int i=0;i<10;i++)
+			System.out.println("hi");
+
+	}
+
+}
